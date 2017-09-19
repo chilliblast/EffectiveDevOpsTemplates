d0b8eb9e4a872bcc76f3d865e7a1bc2bc891cb6d
System Administrator <root@Macbooks-MacBook-Air.local>
2017-09-19 07:08:33 +0100

    Allowing Jenkins to interact with CodePipeline

--- jenkins-cf-template.py
+++ jenkins-cf-template.py
@@ -101,6 +101,18 @@ ud = Base64(Join('\n', [
     "echo '*/10 * * * * {}' > /etc/cron.d/ansible-pull".format(AnsiblePullCmd)
 ]))
 
+t.add_resource(IAMPolicy("Policy", 
+    PolicyName="AllowCodePipeline", 
+    PolicyDocument=Policy( 
+        Statement=[ 
+            Statement(Effect=Allow, 
+                Action=[Action("codepipeline", "*")], 
+                Resource=["*"]) 
+        ] 
+    ), 
+    Roles=[Ref("Role")] 
+))
+
 t.add_resource(ec2.Instance(
     "instance",
     ImageId="ami-ebd02392",
